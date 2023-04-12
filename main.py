ed13ba0eab307cdc1b341c192ec80c0955c83c86
Meluhlah <lidor94@gmail.com>
2023-04-12 12:19:13 +0300

    Bugs fixed

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import sys
 
 # * cell value -> at which cell the item is located at, example: A7
 
-TARGET_COLUMN = "D"  # Which column the data will be inserted at.
-SOURCE_COLUMN = 4  # Location of cell value from source file.
+SOURCE_COLUMN = 'D'  # Location of cell value from source file.
+MAX_ROWS = 500
+MAX_COLUMNS = 500
+TARGET_COL = 4  # In which column the cell location will be inerted at
+DESCRIPTION_CELLS = ['Description', 'Manufacturer', 'MPN', 'QTY', 'CELL']
 
 
 def run(stock_file, bom_file):
-    stock_wb = load_workbook(filename=stock_file)
+    stock_wb = load_workbook(filename=stock_file, read_only=True)
     stock_sheetnames = stock_wb.sheetnames
     bom_wb = load_workbook(filename=bom_file)
     stock_dict = get_dict(stock_wb, stock_sheetnames)
@@ -17,27 +20,27 @@ def run(stock_file, bom_file):
 
 def get_dict(workbook, sheetnames):
     stk_dict = {}
-    for sheet in sheetnames:  # iterate through all the sheets in a wb
-        workbook.active = workbook[sheet]
+    for i, sheet in enumerate(sheetnames):  # iterate through all the sheets in a wb
+        workbook.active = i
         ws = workbook.active
-        for cell in ws.iter_rows(min_row=2, max_row=100, min_col=2, max_col=2):
-            location = ws.cell(column=SOURCE_COLUMN, row=cell[0].row).value
-            stk_dict[location] = cell[0].internal_value
+        for row in ws.iter_rows(min_row=0, max_row=100, min_col=2, max_col=2):
+            for cell in row:
+                if type(cell).__name__ != 'MergedCell' and cell.value is not None and (
+                        cell.value not in DESCRIPTION_CELLS):
+                    cell_info_location = SOURCE_COLUMN + str(cell.row)
+                    cell_location = ws[cell_info_location].value
+                    stk_dict[cell.value] = cell_location
     return stk_dict
 
 
 def insert_cell_location(bom_wb, stock_dict, bom_file):
     bom_ws = bom_wb.active
     bom_ws.cell(row=1, column=4).value = "CELL"  # Write a new column header "CELL"
-    for cell in bom_ws.iter_rows(min_row=2, max_row=100, min_col=2, max_col=2):
-        if cell is not None:
-            for key, val in stock_dict.items():
-                if cell[0].internal_value == val:
-                    destination = str(TARGET_COLUMN + str(cell[0].row))  # Set the cell location to be written
-                    bom_ws[destination] = key
-                    break
-        else:
-            continue
+    for row in bom_ws.iter_rows(min_row=2, max_row=100, min_col=2, max_col=2):
+        for cell in row:
+            if cell.value is not None and (cell.value in stock_dict.keys()):
+                insert_location = SOURCE_COLUMN + str(cell.row)
+                bom_ws[insert_location] = stock_dict[cell.value]
     bom_wb.save(bom_file)
 
 
